db6418e98ac55457428bffa7de0b43493dbca283
Marc.A <marcalberopascual@gmail.com>
2023-11-29 17:32:23 +0100

    done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -7,7 +7,7 @@ const Movie = require("../models/Movie.model");
 router.get("/", async(req, res, next) => {
     try{
         const allMovies = await Movie.find();
-        res.send(allMovies);
+        res.status(200).send(allMovies);
     }
     catch(error){
         console.log(error)
@@ -15,6 +15,15 @@ router.get("/", async(req, res, next) => {
 
   
 });
+router.get("/filter", async(req, res, next) =>{
+    try{
+        const genres = req.body;
+        const filteredMovies = await Movie.find({genre: {$in: filteredMovies}})
+        res.status(200).send(filteredMovies);
+    }catch(error){
+        console.error(error)
+    }
+})
 
     
 
