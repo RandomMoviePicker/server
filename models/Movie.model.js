e67a1ac1be7a65face32a8459123a45509818657
Marc.A <marcalberopascual@gmail.com>
2023-11-29 11:05:41 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const movieSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+    },
+  }
+ 
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
--- models/User.model.js
+++ models/User.model.js
@@ -3,28 +3,25 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    email: {
-      type: String,
-      required: [true, "Email is required."],
-      unique: true,
-      lowercase: true,
-      trim: true,
+    adult: {
+      type: Boolean,
+      required: [true, "Adult is required."],
     },
-    password: {
-      type: String,
-      required: [true, "Password is required."],
-    },
-    name: {
+    title: {
       type: String,
       required: [true, "Name is required."],
     },
+    overview: String,
+    genre: {
+      type: [String],
+      required: [true, "genre is required"]
+    },
+    imageUrl: String,
+    originalLanguage: String,
+    releaseDate: String,
   },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
 );
 
-const User = model("User", userSchema);
+const Movie = model("Movie", movieSchema);
 
-module.exports = User;
+module.exports = Movie;
