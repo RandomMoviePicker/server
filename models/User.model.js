3c5017dd22276a8115e93600bea6dbba57c063f0
Alba Garcia Gonzalez <garciagonzalezalba@gmail.com>
2023-11-29 12:52:41 +0100

    model modified

--- models/User.model.js
+++ models/User.model.js
@@ -1,27 +1,27 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
+const movieSchema = new Schema(
   {
-    adult: {
-      type: Boolean,
-      required: [true, "Adult is required."],
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    title: {
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    name: {
       type: String,
       required: [true, "Name is required."],
     },
-    overview: String,
-    genre: {
-      type: [String],
-      required: [true, "genre is required"]
-    },
-    imageUrl: String,
-    originalLanguage: String,
-    releaseDate: String,
-  },
+  }
+ 
 );
 
-const Movie = model("Movie", movieSchema);
+const User = model("User", userSchema);
 
-module.exports = Movie;
+module.exports = User;
