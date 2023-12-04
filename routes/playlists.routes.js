730309861946f48abec234b77018261d25410bce
Alba Garcia Gonzalez <garciagonzalezalba@gmail.com>
2023-12-04 21:15:15 +0100

    playlists dropdown

--- routes/playlists.routes.js
+++ routes/playlists.routes.js
@@ -28,6 +28,7 @@ const trimedString = name.trim()
 router.get("/", async (req, res, next) => {
     
     try {
+        console.log("llego")
         const { userId } = req.query;
         const data = await Playlist.find({ owner: userId });
         res.status(200).send(data)
